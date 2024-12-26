edfc5c5d92a71a4901a8201c8df12a570b1954e5
JuniorFogassi <junior.fogassi@digihub.com.br>
2024-12-26 12:05:19 -0300

    estilização de componentes

---
+++ src/styles/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+    purple: {
+        BlueViolet: "#8A2BE2",
+        Indigo: "#4B00B2",
+        Purple: "#A020F0"
+    },
+
+    gray: {
+        DarkGray: "#A9A9A9"
+    }
+}
