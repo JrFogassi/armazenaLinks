0100dd0022f0d0cdd302fdde58aaf027fdc941ff
JuniorFogassi <junior.fogassi@digihub.com.br>
2024-12-30 15:30:09 -0300

    Instalando o AsyncStorage e criando os componentes assíncronos de listar e salvar

---
+++ src/storage/link-storage.ts
@@ -0,0 +1,35 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const LINKS_STORAGE_KEY = "links-storage"
+
+type LinkStorage = {
+    id: string,
+    name: string,
+    url: string,
+    category: string,
+}
+
+//Utilizamos funções assíncronas quando estamos lidando com dados que não dependem apenas de nossa aplicação.
+
+async function get(): Promise<LinkStorage[]> {
+    const storage = await AsyncStorage.getItem(LINKS_STORAGE_KEY)
+    //Se tiver conteúdo dentro da variável storage, retorna isso em JSON, se não, traz um objeto vazio
+    const response = storage ? JSON.parse(storage) : []
+
+    return response
+}
+
+async function save(newLink : LinkStorage) {
+    try {
+        const storage = await get()
+        //JSON.stringfy faz a conversão de array para string.
+        const updated = JSON.stringify([...storage, newLink])
+
+        await AsyncStorage.setItem(LINKS_STORAGE_KEY, updated)
+
+    } catch (error) {
+        throw error
+    }
+}
+
+export const LinkStorage = {get, save}
