10b2e7bb485cadbdbc5e2f82fb80033cd9aff3c2
JuniorFogassi <junior.fogassi@digihub.com.br>
2024-12-31 08:59:12 -0300

    Removendo links

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -43,6 +43,26 @@ export default function Index(){
         setLink(selected)
     }
 
+    async function linkRemove() {
+        try {
+          await linkStorage.remove(link.id) 
+          getLinks()
+          setShowModal(false)
+        } catch (error) {
+        Alert.alert("Erro", "Não foi possível excluir")
+        }
+    }
+    
+    
+    function handleRemove() {
+        
+            Alert.alert("Excluir", "Deseja realmente excluir?", [
+                { style: "cancel", text: "Não"},
+                { text: "Sim", onPress: linkRemove },
+            ])
+       
+    }
+
     useFocusEffect(useCallback(() => {
         getLinks()
     }, [category])
@@ -94,7 +114,7 @@ export default function Index(){
                         <Text style={styles.modalUrl}>{link.url}</Text>
 
                         <View style={styles.modalFooter}>
-                            <Option name="Excluir" icon="delete" variant="secondary" />
+                            <Option name="Excluir" icon="delete" variant="secondary" onPress={handleRemove}/>
                             <Option name="Abrir" icon="language" />
                         </View>
                     </View>
